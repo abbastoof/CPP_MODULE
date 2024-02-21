4fcb2d933fbd08ed35642043426b8c58c8c1f87d
abbastoof <abbas.toof@gmail.com>
2024-02-21 17:51:17 +0200

    fixing date for leap year and checked based on csv file

--- C++/C++Module09/ex00/src/main.cpp
+++ C++/C++Module09/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: atoof <atoof@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 14:26:09 by atoof             #+#    #+#             */
-/*   Updated: 2024/02/21 17:14:50 by atoof            ###   ########.fr       */
+/*   Updated: 2024/02/21 17:46:12 by atoof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,18 @@ int main(int argc, char **argv)
 	{
 		std::cerr << e.what() << std::endl;
 	}
+	(void)argc;
+	(void)argv;
 	// read file from command line
-	if (argc == 2)
-	{
-		try
-		{
+	// if (argc == 2)
+	// {
+	// 	try
+	// 	{
 			
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
-		
+	// 	}
+	// 	catch(const std::exception& e)
+	// 	{
+	// 		std::cerr << e.what() << '\n';
+	// 	}
 	return 0;
 }
