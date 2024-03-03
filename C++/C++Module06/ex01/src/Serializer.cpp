3159f04aac31c256405b6ef9be505ef0a12f68fc
abbastoof <abbas.toof@gmail.com>
2024-03-03 19:11:25 +0200

    make sure everything has been pushed

--- C++/C++Module06/ex01/src/Serializer.cpp
+++ C++/C++Module06/ex01/src/Serializer.cpp
@@ -6,7 +6,7 @@
 /*   By: atoof <atoof@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 23:39:40 by atoof             #+#    #+#             */
-/*   Updated: 2024/02/13 15:41:16 by atoof            ###   ########.fr       */
+/*   Updated: 2024/03/03 17:59:59 by atoof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 // unsigned integer types large enough to hold a pointer
 uintptr_t Serializer::serialize(Data* ptr)
 {
-	return reinterpret_cast<uintptr_t>(ptr);
+	return reinterpret_cast<uintptr_t>(ptr); // Converts a pointer to data into an unsigned integer to safely store the pointer's address.
 }
 
 Data *Serializer::deserialize(uintptr_t raw)
 {
-	return reinterpret_cast<Data *>(raw);
+	return reinterpret_cast<Data *>(raw); // Converts an unsigned integer back into a pointer to Data, restoring the original pointer from its stored address.
 }
 
 Serializer::Serializer() {}
