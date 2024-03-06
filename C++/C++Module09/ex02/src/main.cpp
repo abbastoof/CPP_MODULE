2e035d294ffe42150e6b95417d9c7fef85ed80e6
Abbas Toof <abbas.toof@gmail.com>
2024-03-06 19:27:01 +0200

    Add files via upload

--- C++/C++Module09/ex02/src/main.cpp
+++ C++/C++Module09/ex02/src/main.cpp
@@ -6,57 +6,54 @@
 /*   By: atoof <atoof@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 13:20:19 by atoof             #+#    #+#             */
-/*   Updated: 2024/03/01 13:27:07 by atoof            ###   ########.fr       */
+/*   Updated: 2024/03/06 19:25:23 by atoof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/PmergeMe.hpp"
 
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    // Check if at least one integer is provided
-    if (argc < 2)
+	if (argc < 2)
 	{
-        std::cerr << "Usage: " << argv[0] << " [list of integers]" << std::endl;
-        return 1;
-    }
+		std::cerr << "Usage: " << argv[0] << " [list of integers]" << std::endl;
+		return 1;
+	}
 
-    std::vector<int> numbers;
+	std::vector<int> numbers;
 	std::deque<int> numbers2;
-    for (int i = 1; i < argc; ++i)
+	for (int i = 1; i < argc; ++i)
 	{
-        std::istringstream iss(argv[i]);
-        int num;
-		if (argv[i][0] == '-')
+		std::istringstream iss(argv[i]);
+		int num;
+		if (!(iss >> num) || !iss.eof())
+		{ // Checks if the conversion failed or there are trailing characters
+			std::cerr << "Invalid input: " << argv[i] << ". Please enter only integers." << std::endl;
+			return 1;
+		}
+		if (num <= 0)
 		{
 			std::cerr << "Invalid input: " << argv[i] << ". Please enter only positive integers." << std::endl;
 			return 1;
 		}
-        if (iss >> num) // if the conversion to int is successful
-		{
-			numbers.push_back(num);
-			numbers2.push_back(num);
-		}
-        else
-		{
-            std::cerr << "Invalid input: " << argv[i] << ". Please enter only integers." << std::endl;
-            return 1;
-        }
-    }
+		numbers.push_back(num);
+		numbers2.push_back(num);
+	}
+
 	std::cout << "Unsorted positive integer sequence: ";
-	for (int i = 1; i < argc; ++i){std::cout <<argv[i] << " ";}
+	for (int i = 1; i < argc; ++i)
+	{
+		std::cout << argv[i] << " ";
+	}
 	std::cout << std::endl;
 
 	std::cout << Colors::BRIGHT_BLUE << "******************** Vector ********************" << Colors::RESET << std::endl;
 	PmergeMe<int, std::vector> sorter;
 	sorter.sortContainer(numbers);
-	
+
 	std::cout << Colors::BRIGHT_BLUE << "******************** deque ********************" << Colors::RESET << std::endl;
 	PmergeMe<int, std::deque> sorter2;
 	sorter2.sortContainer(numbers2);
 
-
-    return 0;
-
+	return 0;
 }
